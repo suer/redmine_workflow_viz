97088cadef5cbd24340f1e7c5a25130c645ec606
suer <sue@nagoya-u.jp>
2010-08-22 00:52:51 +0900

    edit description

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_workflow_viz do
   name 'Redmine Workflow Viz plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'R.SUETSUGU'
+  description 'visualization of workflow definition'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://github.com/suer/redmine_workflow_viz'
+  author_url 'http://d.hatena.ne.jp/suer'
 end
